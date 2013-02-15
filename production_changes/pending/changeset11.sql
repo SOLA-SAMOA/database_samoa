b08f2cc300e2e9d6ffd7cc5160ab68fce19600d3
Andrew McDowell <armcdowell@hotmail.com>
2013-02-15 17:52:37 +1300

    Unable to change Court Grants [#79 state:fixed responsible:none milestone:v2.1.2]
    Fixed in changeset11. The LRS migration did not bring the court grant Folio reference
    correctly into SOLA. Have updated so that all Court Grants now have the Lot/Parcel Number
    of CTGT and the Plan number is the grant number. E.g. CTGT/593. This will allow them to
    be added as Properties to the application.

--- production_changes/pending/changeset11.sql
+++ production_changes/pending/changeset11.sql
@@ -1,5 +1,56 @@
 -- 18 Feb 2012
 
+-- #78 Reset the 3 lots of 10439 from historic to current
+UPDATE administrative.ba_unit 
+SET status_code = 'current'
+WHERE name_lastpart = '10439';
 
 
+-- #79 Fix the folio references for the Court Grants and some of the
+-- old V references. 
+UPDATE administrative.ba_unit
+SET name_firstpart = 'CTGT',
+    name_lastpart = regexp_replace(name_lastpart, '\D*',  ''), 
+    name = 'CTGT/' ||  regexp_replace(name_lastpart, '\D*',  '')
+WHERE name_lastpart LIKE 'CTGT%'
+AND name_lastpart != 'CTGTCTGT320'
+AND status_code != 'pending';
 
+UPDATE administrative.ba_unit
+SET name_firstpart = 'CTGT',
+    name_lastpart = '320(2)', 
+    name = 'CTGT/320(2)' 
+WHERE name_lastpart = 'CTGTCTGT320';
+
+UPDATE administrative.ba_unit
+SET name_firstpart = 'V52',
+    name_lastpart = '57', 
+    name = 'V52/57' 
+WHERE name_lastpart = 'V5257';
+
+UPDATE administrative.ba_unit
+SET name_firstpart = 'V52',
+    name_lastpart = '142', 
+    name = 'V52/142' 
+WHERE name_lastpart = 'V52142';
+
+UPDATE administrative.ba_unit
+SET name_firstpart = 'V52',
+    name_lastpart = '51', 
+    name = 'V52/51' 
+WHERE name_lastpart = 'V5251';
+
+UPDATE administrative.ba_unit
+SET name_firstpart = 'V53',
+    name_lastpart = '92', 
+    name = 'V53/92' 
+WHERE name_lastpart = 'V5392';
+
+-- Fix the application property on application 39032
+UPDATE application.application_property 
+SET name_firstpart = 'CTGT', 
+    name_lastpart = '593',
+    verified_exists = true,
+    verified_location = true,
+    ba_unit_id = (SELECT id FROM administrative.ba_unit WHERE name = 'CTGT/593')
+WHERE application_id IN (SELECT id FROM application.application WHERE nr = '39032');
