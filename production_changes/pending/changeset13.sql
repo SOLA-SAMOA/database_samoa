5523e240d42399719f00ea2879be9ac2ad1e34da
amcdowell <armcdowell@hotmail.com>
2013-03-01 09:52:40 +1300

    Update Glassfish Keystores
    Fixed in v2.1.4. Updated Glassfish keystores removing
    any references to the new keystores or the keystore
    passwords. Also updated the Web Start Code Signing
    keystore in a similar manner.
    
    [#90 state:fixed responsible:none milestone:v2.1.4]

--- production_changes/pending/changeset13.sql
+++ production_changes/pending/changeset13.sql
@@ -1,3 +1,11 @@
--- XX Mar 2012
+-- 4 Mar 2012
+
+-- Update Training, test the deployment, then update Production. 
+-- Update Glassfish Keystore certificates by copying cacerts.jks and keystore.jks
+-- From Dropbox\Keystores folder to the config directory of the Glassfish SOLA 
+-- Domain. Run the asadmin change-master-password subcommand and set the 
+-- new Glassfish Master Password to match the storepass in 
+-- Dropbox\Keystores\settings.xml file. 
+-- <Glassfish Install Dir>\bin\asadmin change-master-password --savemasterpassword=true --domaindir <SOLA Glassfish Domain Dir>
 
 
