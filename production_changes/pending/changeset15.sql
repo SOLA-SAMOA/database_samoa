f8b05658b33d2459450686de119962c82a705b07
amcdowell <armcdowell@hotmail.com>
2013-03-27 20:06:31 +1300

    Add Village

---
+++ production_changes/pending/changeset15.sql
@@ -0,0 +1,5 @@
+-- 28 Mar 2012
+-- #95 Add Fiaga Village
+INSERT INTO administrative.ba_unit (id, type_code, name, name_firstpart, name_lastpart, status_code, change_user)
+VALUES ('LOC500', 'administrativeUnit', 'Fiaga/Village', 'Fiaga', 'Village', 'current', 'andrew'); 
+
