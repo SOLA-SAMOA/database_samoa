b58bf499dfcb978b18dd742773431619c3155019
amcdowell <armcdowell@hotmail.com>
2013-05-24 11:50:48 +1200

    Fixes for ticket 100 - Changeset17.sql
    [#100 state:fixed responsible:none milestone:v2.1.5]

--- production_changes/pending/changeset17.sql
+++ production_changes/pending/changeset17.sql
@@ -1,4 +1,27 @@
--- 20 May 2012
--- Fix naming for lots on plan 11083. Parcels for 11038 were incorrectly migrated into 
--- SOLA as parcels 11083.
-update cadastre.cadastre_object SET name_lastpart = '11038-' where name_lastpart = '11083'; 
+-- 24 May 2012
+-- Fix naming for lots on plan 11083. Add LOT to the first part of the name
+UPDATE cadastre.cadastre_object
+SET name_firstpart = ' ' || name_firstpart
+WHERE name_lastpart = '11083'
+AND status_code = 'historic';
+
+UPDATE cadastre.cadastre_object
+SET name_firstpart = 'LOT ' || TRIM(name_firstpart)
+WHERE name_lastpart = '11083'
+AND status_code = 'current';
+
+-- Fix the area for the parcel 190/3776
+UPDATE cadastre.spatial_value_area
+SET size = 20235
+WHERE spatial_unit_id IN 
+ (SELECT id from cadastre.cadastre_object
+  WHERE name_lastpart = '3776'
+  AND name_firstpart = '190');
+
+-- Fix the area for the ba unit 190/3776
+UPDATE administrative.spatial_value_area
+SET size = 20235
+WHERE spatial_unit_id IN 
+ (SELECT id from cadastre.cadastre_object
+  WHERE name_lastpart = '3776'
+  AND name_firstpart = '190');
